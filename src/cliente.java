d5d001ac54bd6da1630a36b1fec141b01da5458d
Jose_Murciano <jmurjur1701@g.educaand.es>
2026-02-11 19:44:19 +0100

    Crear clase cliente (nombre, ID, saldo). Closes #3

--- src/cliente.java
+++ src/cliente.java
@@ -1,2 +1,19 @@
 public class cliente {
-}
+    private String nombre;
+    private String id;
+    private double saldo;
+
+
+    public cliente(String nombre, String id, double saldo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getId() { return id; }
+    public double getSaldo() { return saldo; }
+
+
+
+    git add Cliente.java}
