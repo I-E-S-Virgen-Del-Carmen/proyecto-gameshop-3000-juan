72f9d4fdab117d61faab82e74ad9d60c5761e3f8
jose4murciano <jose4murciano@gmail.com>
2026-02-11 19:55:58 +0100

    Añadir Nombre, id , Saldo

--- src/cliente.java
+++ src/cliente.java
@@ -1,2 +1,16 @@
 public class cliente {
+private String nombre;
+    private String id;
+    private double saldo;
+
+    
+    public Cliente(String nombre, String id, double saldo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.saldo = saldo;
+    }
+
+    public String getNombre() { return nombre; }
+    public String getId() { return id; }
+    public double getSaldo() { return saldo; }
 }
