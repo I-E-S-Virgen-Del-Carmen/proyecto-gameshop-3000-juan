afbb5745b557c5ffbbec45748d3b60331c9570dd
Oscar_Garcia <ogarmun1005@g.educaand.es>
2026-02-11 20:03:53 +0100

    UPDATE

--- src/juego.java
+++ src/juego.java
@@ -1,2 +1,72 @@
 public class juego {
+    // ATRIBUTOS
+    private String titulo;
+    private double precio;
+    private String genero;
+    private boolean disponible;
+
+    // CONSTRUCTOR
+    public Juego(String titulo, double precio, String genero) {
+        this.titulo = titulo;
+        this.precio = precio;
+        this.genero = genero;
+        this.disponible = true; // Cuando se crea, está disponible
+    }
+
+    // GETTERS
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public boolean isDisponible() {
+        return disponible;
+    }
+
+    // SETTERS (opcionales, pero útiles)
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    // MÉTODO PARA ALQUILAR
+    public void alquilar() {
+        if (disponible) {
+            disponible = false;
+            System.out.println("El juego \"" + titulo + "\" ha sido alquilado.");
+        } else {
+            System.out.println("El juego \"" + titulo + "\" no está disponible.");
+        }
+    }
+
+    // MÉTODO PARA DEVOLVER
+    public void devolver() {
+        disponible = true;
+        System.out.println("El juego \"" + titulo + "\" ha sido devuelto.");
+    }
+
+    // toString()
+    @Override
+    public String toString() {
+        return "Juego{" +
+                "titulo='" + titulo + '\'' +
+                ", precio=" + precio +
+                ", genero='" + genero + '\'' +
+                ", disponible=" + (disponible ? "Sí" : "No") +
+                '}';
+    }
 }
