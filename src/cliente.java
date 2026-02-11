62bd0534b70caeb1e59019d694e5dad6eda4d9e2
Javier <jdiacas0408@g.educaand.es>
2026-02-11 20:20:22 +0100

    Añade el menu interactivo a main

--- src/cliente.java
+++ src/cliente.java
@@ -1,2 +1,23 @@
-public class cliente{
+public class Cliente {
+    private String nombre;
+    private String id;
+    private double saldo;
+
+    public Cliente(String nombre, String id, double saldo) {
+        this.nombre = nombre;
+        this.id = id;
+        this.saldo = saldo;
+    }
+
+    // Método para la lógica de alquiler (Fase 4)
+    public void descontarSaldo(double monto) {
+        this.saldo -= monto;
+    }
+
+    public double getSaldo() { return saldo; }
+
+    @Override
+    public String toString() {
+        return "Cliente: " + nombre + " (ID: " + id + ") | Saldo: " + saldo + "€";
+    }
 }
--- src/juego.java
+++ src/juego.java
@@ -1,2 +1,20 @@
-public class juego {
+public class Juego {
+    private String titulo;
+    private double precio;
+    private String genero;
+
+    public Juego(String titulo, double precio, String genero) {
+        this.titulo = titulo;
+        this.precio = precio;
+        this.genero = genero;
+    }
+
+    // Getters para que el Dev C pueda usar los datos en el menú
+    public String getTitulo() { return titulo; }
+    public double getPrecio() { return precio; }
+
+    @Override
+    public String toString() {
+        return "Juego: " + titulo + " | Género: " + genero + " | Precio: " + precio + "€";
+    }
 }
